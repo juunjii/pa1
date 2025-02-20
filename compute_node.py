7a205f1baacf59e80cfa59305929448c71e247bb
Jun Hao Cheh <cheh0011@umn.edu>
2025-02-20 10:11:23 -0600

    Added skeletion functions to compute node

---
+++ compute_node.py
@@ -0,0 +1,15 @@
+from ML import *
+import numpy as np
+
+class ComputeNode:
+    def __init__(self, load_probability):
+        self.load_probability = float(load_probability)
+
+    def trainMLP(self, weights, data, eta, epochs):
+        pass
+
+    def loadInjection():
+        pass
+    
+    def rejectTask():
+        pass
