1f1bafd1456142bfea4e315345828adaf8708433
Jun Hao Cheh <cheh0011@umn.edu>
2025-02-20 19:01:43 -0600

    Added thrift file for compute node

--- compute_node.py
+++ compute_node.py
@@ -1,15 +1,28 @@
 from ML import *
-import numpy as np
+import sys
+import time
+import random
+import glob
+sys.path.append('gen-py')
+sys.path.insert(0, glob.glob('../../thrift-0.19.0/lib/py/build/lib*')[0])
 
-class ComputeNode:
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+class ComputeNodeHandler:
     def __init__(self, load_probability):
         self.load_probability = float(load_probability)
 
     def trainMLP(self, weights, data, eta, epochs):
         pass
 
-    def loadInjection():
-        pass
+    def loadInjection(self):
+        if random.random() < self.load_probability:
+            time.sleep(3)  # 3-second delay
     
-    def rejectTask():
-        pass
+    def rejectTask(self):
+        return random.random() < self.load_probability 
+
+
---
+++ coordinator_node.thrift
@@ -0,0 +1,11 @@
+struct WeightMatrices {
+    1: list<list<double>> V,
+    2: list<list<double>> W
+}
+
+
+service coordinator 
+{ 
+double train(1:string dir, 2:i32 rounds, 3:i32 epochs, 
+4:i32 h, 5:i32 k, 6:double eta) 
+}
